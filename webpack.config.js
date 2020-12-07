611a3dc984b38c2d0ddb79eeb9cb07b1d91f296e
YS <ribe1918s@gmail.com>
2020-12-07 23:05:37 +0900

    Initialize Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,53 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: path.join(__dirname, 'index.tsx'),
-    output: {
-        // publicPath: '/',
-        // path: path.join(__dirname, '/dist'),
-        // filename: 'bundle.[hash].js'
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
-                        ]
-                    }
-                },
-            },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: {
-                            minimize: true,
-                        },
-                    },
-                ],
-            },
+  entry: path.join(__dirname, '/src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ]
+          }
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
         ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        })
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
     ],
-    devServer: {
-        host: 'localhost',
-        port: 8081,
-        open: true,
-        historyApiFallback: true,
-    },
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    historyApiFallback: true,
+  },
 }

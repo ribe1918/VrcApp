a6c0c7feded5077818ea63d22aaf0eaa2a63882a
ribe <ribe1918s@gmail.com>
2020-12-10 10:56:03 +0900

    Change Element

--- src/view/layout/Navigation.tsx
+++ src/view/layout/Navigation.tsx
@@ -1,22 +1,50 @@
 import { InfoIcon } from '@chakra-ui/icons'
-import { Center, IconButton, Link, List, ListIcon, ListItem } from '@chakra-ui/react'
+import { VStack ,Center, IconButton, Link, List, ListIcon, ListItem } from '@chakra-ui/react'
 import React from 'react'
 
 const Navigation: React.FC = () => {
   return (
-        <List spacing={3}>
-          <Link>
-            <ListItem>
-              <IconButton
-                variant="outline"
-                colorScheme="teal"
-                aria-label="Send email"
-                size='lg'
-                icon={<InfoIcon />}
-              />
-            </ListItem>
-          </Link>
-        </List>
+    <VStack
+      spacing={4}
+      p={2}
+    >
+      <IconButton
+        variant="outline"
+        colorScheme="teal"
+        aria-label="UserInfo"
+        size='lg'
+        fontSize="20px"
+        to='/'
+        icon={<InfoIcon />}
+      />
+
+      <IconButton
+        variant="outline"
+        colorScheme="teal"
+        aria-label="Send email"
+        size='lg'
+        fontSize="20px"
+        icon={<InfoIcon />}
+      />
+
+      <IconButton
+        variant="outline"
+        colorScheme="teal"
+        aria-label="Send email"
+        size='lg'
+        fontSize="20px"
+        icon={<InfoIcon />}
+      />
+
+      <IconButton
+        variant="outline"
+        colorScheme="teal"
+        aria-label="Send email"
+        size='lg'
+        fontSize="20px"
+        icon={<InfoIcon />}
+      />
+    </VStack>
   )
 }
 
